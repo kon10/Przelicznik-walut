4a8efb4f8139ba3e667d370c7b637af261c4b4c6
Kontent <czepiel.marcin@outlook.com>
2021-02-16 17:04:07 +0100

    add files

---
+++ index.js
@@ -0,0 +1,40 @@
+axios.defaults.baseURL = 'http://api.nbp.pl/api/exchangerates/rates/A';
+
+let resultBtn = document.querySelector('.result');
+resultBtn.addEventListener('click', calculateRate);
+
+function calculateRate() {
+
+  let selCurrency = document.getElementById('sel-currency').value;
+  let errorInfo = document.querySelector('.info');
+  
+  axios.get(`/${selCurrency}`)
+    .then(response => {
+      let lastTblDate = response.data.rates[0].effectiveDate;
+      let lastRate = response.data.rates[0].mid;
+      let code = response.data.code;
+      refreshValOutput(lastRate, errorInfo, code, lastTblDate);
+    })
+};
+
+function refreshValOutput(lastRate, errorInfo, code, lastTblDate) {
+  let insertVal = document.querySelector('.val').value;
+  insertVal = insertVal.replace(",", ".");
+  insertVal = Number.parseFloat(insertVal);
+  insertVal = Math.round(insertVal * 100) / 100;
+
+  if (isNaN(insertVal)) {
+    errorInfo.style.setProperty('visibility', 'visible');
+  }
+  else {
+    let totalInCurr = insertVal * lastRate
+    const totalVal = document.querySelector('.value');
+    totalVal.innerText = totalInCurr.toFixed(2).replace(".", ",") + ' PLN';
+    refreshLastRateInfo(code, lastRate, lastTblDate);
+    errorInfo.style.setProperty('visibility', 'hidden');
+    return totalInCurr;
+  };
+};
+
+
+
